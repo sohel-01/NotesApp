925bc036413e69f84e2a6a65b0b5733e06577dc5
mohit <mohit_nitd@outlook.com>
2018-12-15 20:50:57 -0500

    Notes

---
+++ app/src/main/java/com/example/kashyap/notesapp/AddVoiceNotes.java
@@ -0,0 +1,245 @@
+package com.example.kashyap.notesapp;
+
+import android.Manifest;
+import android.content.ContentValues;
+import android.content.pm.PackageManager;
+import android.database.sqlite.SQLiteDatabase;
+import android.media.*;
+import android.net.Uri;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.*;
+
+import java.io.*;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+public class AddVoiceNotes extends AppCompatActivity implements View.OnClickListener {
+
+    // Database name
+    private static final String DATABASE_NAME = "Notes";
+
+    // SQL Create statement
+    private static final String CREATE_NOTES_TABLE = "CREATE TABLE IF NOT EXISTS tbl_notes (notesid INTEGER " +
+            "PRIMARY KEY AUTOINCREMENT, subjectid TEXT, title TEXT, datetime TEXT, location TEXT, notes TEXT);";
+
+    // Database instance
+    private SQLiteDatabase mDatabase;
+
+    Button btnRecord;
+    Button btnPlay;
+    Button btnSave;
+
+    String RECORDED_FILE;
+    MediaRecorder audioListener;
+    MediaPlayer player;
+
+    String strNotesTitle;
+    String strVoiceNotePath;
+
+    private ArrayAdapter<String> listSubject;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_voice_notes);
+
+        strNotesTitle = getIntent().getExtras().getString("NotesTitle");
+
+        //RECORDED_FILE = "/NotesApp/VoiceNotes/" + strNotesTitle + "/audio.mp4";
+
+        DateFormat dateFormat = new SimpleDateFormat("MMMddyyyyHHmmss");
+        RECORDED_FILE = "/" + strNotesTitle.replace(" ", "") + dateFormat.format(new Date()) + "audio.3gp";
+
+        openOrCreateDatabase();
+        gatherControls();
+
+        btnPlay.setEnabled(false);
+        btnSave.setEnabled(false);
+    }
+
+    public void openOrCreateDatabase() {
+        mDatabase = openOrCreateDatabase(DATABASE_NAME, SQLiteDatabase.CREATE_IF_NECESSARY, null);
+
+        mDatabase.setLocale(Locale.getDefault());
+        mDatabase.setVersion(1);
+
+        mDatabase.execSQL(CREATE_NOTES_TABLE);
+    }
+
+    private void gatherControls() {
+        btnRecord = (Button)findViewById(R.id.btnRecord);
+        btnRecord.setOnClickListener(this);
+
+        btnPlay = (Button)findViewById(R.id.btnPlay);
+        btnPlay.setOnClickListener(this);
+
+        btnSave = (Button)findViewById(R.id.btnSave);
+        btnSave.setOnClickListener(this);
+    }
+
+    protected void saveVoiceNotes() {
+
+        ContentValues values = new ContentValues();
+
+        values.put("notesid", strNotesTitle);
+        values.put("mediaPath", strVoiceNotePath);
+        values.put("mediaType", "Voice");
+
+        mDatabase.insert("tbl_media", null, values);
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        Button b = (Button)v;
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED) {
+
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.RECORD_AUDIO}, 10);
+
+        }
+
+        switch (b.getId()) {
+            case R.id.btnRecord:
+                if (btnRecord.getText().equals("Record")) {
+                    if (audioListener == null) {
+                        audioListener = new MediaRecorder();
+                    }
+
+                    // Fully qualified path name. In this case, we use the Files
+                    // subdir
+                    String pathForAppFiles = getFilesDir().getAbsolutePath();
+                    pathForAppFiles += RECORDED_FILE;
+                    Log.d("Audio filename:", pathForAppFiles);
+
+                    //File destination = new File(pathForAppFiles);
+                    //destination.mkdir();
+
+                    File filename = new File(pathForAppFiles);
+                    if (filename.exists()) {
+                        filename.delete();
+                    }
+//
+//                    try {
+//                        filename.createNewFile();
+//
+//                    } catch (Exception e) {
+//                        e.printStackTrace();
+//                    }
+
+                    try {
+                        audioListener.setAudioSource(MediaRecorder.AudioSource.MIC);
+                        audioListener.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+                        audioListener.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
+                        audioListener.setOutputFile(pathForAppFiles);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+
+
+                    try {
+                        audioListener.prepare();
+                        audioListener.start();
+                    } catch (Exception e) {
+                        Log.e("Audio", "Failed to prepare and start audio recording", e);
+                    }
+
+                    btnRecord.setText("Stop");
+
+                    btnPlay.setEnabled(false);
+                    btnSave.setEnabled(false);
+                }
+                else if (btnRecord.getText().equals("Stop")) {
+
+                    if (ActivityCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+
+                        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 10);
+
+                    }
+                    if (audioListener == null)
+                        return;
+
+                    audioListener.stop();
+                    audioListener.release();
+                    audioListener = null;
+
+                    String pathForAppFiles = getFilesDir().getAbsolutePath();
+                    pathForAppFiles += RECORDED_FILE;
+                    Log.d("Audio filename:", pathForAppFiles);
+
+                    strVoiceNotePath = pathForAppFiles;
+
+                    ContentValues values = new ContentValues(10);
+
+                    values.put(MediaStore.MediaColumns.TITLE, "RecordedAudio");
+                    values.put(MediaStore.MediaColumns.DATE_ADDED, System.currentTimeMillis() / 1000);
+                    values.put(MediaStore.MediaColumns.MIME_TYPE, "audio/mp4");
+                    values.put(MediaStore.MediaColumns.DATA, pathForAppFiles);
+
+                    Uri uri = MediaStore.Audio.Media.getContentUriForPath(getFilesDir().getAbsolutePath() + pathForAppFiles);
+                    getContentResolver().delete(uri, null, null);
+
+                    Uri audioUri = getContentResolver().insert(MediaStore.Audio.Media.EXTERNAL_CONTENT_URI, values);
+                    if (audioUri == null) {
+                        Log.d("Audio", "Content resolver failed");
+                        return;
+                    }
+
+                    // Force Media scanner to refresh now. Technically, this is
+                    // unnecessary, as the media scanner will run periodically but
+                    // helpful for testing.
+                    //Log.d("Audio URI", "Path = " + audioUri.getPath());
+                    //sendBroadcast(new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE, audioUri));
+
+                    //RingtoneManager.setActualDefaultRingtoneUri(getApplicationContext(), RingtoneManager.TYPE_RINGTONE, audioUri);
+
+                    btnRecord.setText("Record");
+
+                    btnPlay.setEnabled(true);
+                    btnSave.setEnabled(true);
+                }
+                break;
+
+            case R.id.btnPlay:
+                player = null;
+                if (player == null) {
+                    player = new MediaPlayer();
+                }
+                try {
+
+                    // Fully qualified path name. In this case, we use the Files
+                    // subdir
+                    String audioFilePath = getFilesDir().getAbsolutePath();
+                    audioFilePath += RECORDED_FILE;
+                    //Log.d("Audio filename:", audioFilePath);
+
+                    player.setDataSource(audioFilePath);
+                    player.prepare();
+                    player.start();
+                } catch (Exception e) {
+                    Log.e("Audio", "Playback failed.", e);
+                }
+                break;
+
+            case R.id.btnSave:
+                if (player != null) {
+                    player.stop();
+                    player.release();
+                    player = null;
+                }
+
+                saveVoiceNotes();
+
+                finish();
+                break;
+        }
+    }
+}
---
+++ app/src/main/java/com/example/kashyap/notesapp/MainActivity.java
@@ -0,0 +1,284 @@
+package com.example.kashyap.notesapp;
+
+import android.Manifest;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.ContentValues;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.*;
+
+import java.util.ArrayList;
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity implements ListView.OnItemClickListener, ListView.OnItemLongClickListener {
+
+    // Database name
+    private static final String DATABASE_NAME = "Notes";
+
+    // SQL Create statement
+    private static final String CREATE_SUBJECT_TABLE = "CREATE TABLE IF NOT EXISTS tbl_subject (subjectid INTEGER PRIMARY KEY AUTOINCREMENT, " +
+            "subjectname TEXT);";
+
+    // Database instance
+    private SQLiteDatabase mDatabase;
+
+    ListView lvSubject;
+
+    private ArrayAdapter<String> listSubject;
+
+    String strDeleteSubject = null;
+
+    private static final String FINE_LOCATION = Manifest.permission.ACCESS_FINE_LOCATION;
+    private static final String COARSE_LOCATION = Manifest.permission.ACCESS_COARSE_LOCATION;
+    private static final String CAM = Manifest.permission.CAMERA;
+    private static final String WRITE = Manifest.permission.WRITE_EXTERNAL_STORAGE;
+    private static final int PERMISSION_CODE = 1234;
+    private Boolean mPermission = false;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        openOrCreateDatabase();
+        gatherControls();
+        getAllPermission();
+        fetchAllSubjects();
+    }
+
+    public void openOrCreateDatabase() {
+        mDatabase = openOrCreateDatabase(DATABASE_NAME, SQLiteDatabase.CREATE_IF_NECESSARY, null);
+
+        mDatabase.setLocale(Locale.getDefault());
+        mDatabase.setVersion(1);
+
+        mDatabase.execSQL(CREATE_SUBJECT_TABLE);
+    }
+
+    private void gatherControls() {
+        lvSubject = (ListView) findViewById(R.id.lvSubject);
+        lvSubject.setOnItemClickListener(this);
+        lvSubject.setLongClickable(true);
+        lvSubject.setOnItemLongClickListener(this);
+    }
+
+    private void fetchAllSubjects() {
+        Cursor c = mDatabase.query("tbl_subject", null, null, null, null, null, null);
+        c.moveToFirst();
+
+        ArrayList<String> arrSubject = new ArrayList<>();
+
+        while (!c.isAfterLast()) {
+            arrSubject.add(c.getString(c.getColumnIndex("subjectname")));
+
+            c.moveToNext();
+        }
+
+        c.close();
+
+        listSubject = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, arrSubject);
+
+        lvSubject.setAdapter(listSubject);
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        if (strDeleteSubject == null) {
+            Intent i = new Intent(getApplicationContext(), NotesList.class);
+            i.putExtra("SubjectName", lvSubject.getItemAtPosition(position).toString());
+            startActivity(i);
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.home_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()) {
+            case R.id.add_new_item:
+
+                final Dialog dialog = new Dialog(this);
+                dialog.setContentView(R.layout.add_subject);
+//                dialog.setTitle("New Note");
+                dialog.getWindow().setLayout(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+
+                Button btnCancel = (Button) dialog.findViewById(R.id.btnCancel);
+                TextView tvTitle = (TextView) dialog.findViewById(R.id.tvTitle);
+                tvTitle.setText("New Subject");
+                final EditText etSubject = (EditText) dialog.findViewById(R.id.etSubject);
+                etSubject.setHint("Enter note title");
+                Button btnSave = (Button) dialog.findViewById(R.id.btnSave);
+
+                btnSave.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        if (saveSubject(etSubject)) {
+                            dialog.dismiss();
+                            fetchAllSubjects();
+                        }
+                    }
+                });
+
+                btnCancel.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        dialog.dismiss();
+                    }
+                });
+
+                dialog.show();
+                break;
+        }
+        return true;
+    }
+
+    private boolean saveSubject(EditText etSubject) {
+        if (etSubject.getText().toString().isEmpty()) {
+            Toast.makeText(getApplicationContext(), "Please enter Subject", Toast.LENGTH_SHORT).show();
+            etSubject.requestFocus();
+            return false;
+        }
+
+        ContentValues values = new ContentValues();
+        values.put("subjectname", etSubject.getText().toString());
+
+        mDatabase.insert("tbl_subject", null, values);
+
+        return true;
+    }
+
+
+
+    private void deleteSubject() {
+        if (strDeleteSubject != null) {
+            String astrArgs[] = {strDeleteSubject.toString()};
+            mDatabase.delete("tbl_subject", "subjectname=?", astrArgs);
+
+            strDeleteSubject = null;
+        }
+    }
+
+    @Override
+    public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+
+        strDeleteSubject = listSubject.getItem(position);
+
+        final Dialog dialog = new Dialog(this);
+        dialog.setContentView(R.layout.menu_delete);
+        dialog.setTitle("Delete");
+        dialog.getWindow().setLayout(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+
+        Button btnDelete = (Button) dialog.findViewById(R.id.btnDelete);
+        Button btnCancel = (Button) dialog.findViewById(R.id.btnCancel);
+
+        btnDelete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                dialog.dismiss();
+                showAlert();
+            }
+        });
+
+        btnCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                strDeleteSubject = null;
+                dialog.dismiss();
+            }
+        });
+
+        dialog.show();
+
+        return false;
+    }
+
+    private void showAlert() {
+        AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(this);
+        dialogBuilder.setTitle("Delete");
+        dialogBuilder.setMessage("Are you sure?");
+        dialogBuilder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogAlert, int which) {
+                deleteSubject();
+                fetchAllSubjects();
+            }
+        });
+
+        dialogBuilder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogAlert, int which) {
+                strDeleteSubject = null;
+                dialogAlert.dismiss();
+            }
+        });
+
+        AlertDialog alert = dialogBuilder.create();
+        alert.show();
+    }
+    private void getAllPermission(){
+        String[] permission = {Manifest.permission.ACCESS_FINE_LOCATION,Manifest.permission.ACCESS_COARSE_LOCATION,Manifest.permission.CAMERA,
+                                Manifest.permission.RECORD_AUDIO,Manifest.permission.WRITE_EXTERNAL_STORAGE
+                                };
+        if(ContextCompat.checkSelfPermission(this.getApplicationContext(),FINE_LOCATION) == PackageManager.PERMISSION_GRANTED){
+            if(ContextCompat.checkSelfPermission(this.getApplicationContext(),COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED){
+                if(ContextCompat.checkSelfPermission(this.getApplicationContext(),CAM) == PackageManager.PERMISSION_GRANTED){
+                    if(ContextCompat.checkSelfPermission(this.getApplicationContext(),WRITE) == PackageManager.PERMISSION_GRANTED){
+                        mPermission = true;
+                    }else {
+                        ActivityCompat.requestPermissions(this,permission,PERMISSION_CODE);
+                    }
+                }else{
+                    ActivityCompat.requestPermissions(this,permission,PERMISSION_CODE);
+                }
+            }else {
+                ActivityCompat.requestPermissions(this,permission,PERMISSION_CODE);
+            }
+        }else {
+            ActivityCompat.requestPermissions(this,permission,PERMISSION_CODE);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        mPermission = false;
+        switch (requestCode){
+            case PERMISSION_CODE:{
+                if(grantResults.length > 0){
+                    for(int i = 0;i < grantResults.length;i++){
+                        if(grantResults[i] != PackageManager.PERMISSION_GRANTED){
+                            mPermission = false;
+                            Log.d("Permission","Falied");
+                            return;
+                        }
+                    }
+                    Log.d("Permission","Granted");
+                    mPermission = true;
+                }
+            }
+        }
+    }
+}
